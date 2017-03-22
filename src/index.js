d8bb88d65d305f1e83316314ef0354dacbc1a331
Luis Nunez <lan.alberto.vkrum@gmail.com>
2017-03-22 10:05:01 -0300

    removing 'http/' from installer request - this is for heroku

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const serverUrl = `${app.get('host')}`;
 server.on('listening', () => {
 	console.log(`Feathers application started on ${serverUrl}`);
 	console.log(`Installing on ${serverUrl}install...`);
-	request(`http://${serverUrl}/install`, (error, response, body) => {
+	request(`${serverUrl}install`, (error, response, body) => {
 		if (!error && response.statusCode == 200) {
 			console.log(body);
 		} else {
